0388fe8405068a0e4502100c2a2196e88a2c01da
Alex <alexkachan@protonmail.com>
2023-05-01 00:33:29 -0500

    adding 3 questions

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <question-box
+    v-for="question in questions"
+    v-bind:question="question"
+    v-on:choice-made="choiceMade"
+    ></question-box>
+  <h2>You would rather...</h2>
+  <li v-for="answer in answers">Have {{ answer }}</li>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuestionBox from './components/WouldYouRatherQuestion.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {QuestionBox,},
+  data() {
+    return {
+      questions: [
+        {
+          id:0,
+          q:"Would you rather have orangutang arms or ostrich legs?",
+          a1:"Orangutang arms",
+          a2:"Ostrich Legs"
+        },
+        {
+          id:1,
+          q:"Would you rather have an anteater tongue or elephant ears?",
+          a1:"Anteater tongue",
+          a2:"Elephant ears"
+        },
+        {
+          id:2,
+          q:"Would you rather take 5 dollars now or 20 dollars in 2 months?",
+          a1:"5 dollars now",
+          a2:"20 dollars in the future"
+        }
+      ],
+      answers: [],
+    }
+  },
+  methods: {
+    choiceMade(question, answer) {
+      this.answers[question]=answer
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background: navy;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: purple;
   margin-top: 60px;
+  background: aquamarine
 }
 </style>
+
---
+++ src/components/WouldYouRatherQuestion.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="wyr">
+        <h2>Would you rather...</h2>
+        <h3>{{ question.q }}</h3>
+        <input type="radio" v-model="choice" v-bind:value="question.a1" v-on:change="choiceMade"><label>{{ question.a1 }}</label>
+        <input type="radio" v-model="choice" v-bind:value="question.a2" v-on:change="choiceMade"><label>{{ question.a2 }}</label>
+    </div>    
+</template>
+
+<script>
+export default {
+    name:'QuestionBox',
+    emits: ['choice-made'],
+    props: {
+        question: Object
+    },
+    data() {
+        return {
+            choice:''
+        }
+    },
+    methods: {
+        choiceMade() {
+            this.$emit('choice-made', this.question.id, this.choice)
+        }
+    }
+}
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.wyr {
+  background: chartreuse
+}
+</style>
